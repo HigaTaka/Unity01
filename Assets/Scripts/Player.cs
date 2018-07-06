2495b0646dafbebd2ef5a17ff41796b8c656cee9
takanori1973 <lr-higashida@lantern-rooms.com>
2018-07-06 12:52:12 +0900

    弾幕ゲームをつくるん

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	GameObject Bullet;
+	GameObject Target;
+	// 弾丸の速度
+	public float speed = 1000;
+
+	// Use this for initialization
+	void Start () {
+		//Resourcesフォルダーから読み込みをする
+		Bullet = Resources.Load("Prefub/P_BulletBIG") as GameObject;
+		//targetを探す
+		Target = GameObject.Find("Enemy");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetButtonDown("Fire1") )
+		{
+			BulletSet();
+		}
+	}
+
+	void BulletSet()
+	{
+		GameObject Obj = Instantiate(Bullet);
+		Obj.transform.position = transform.position;
+		//自分の向きをターゲットの正面に向ける
+		transform.LookAt(Target.transform);
+
+		Vector3 force;
+		force = this.gameObject.transform.forward * speed;
+		// Rigidbodyに力を加えて発射
+		Obj.GetComponent<Rigidbody>().AddForce(force);
+		//
+		Destroy( Obj, 3.0f);
+	}
+}
---
+++ Assets/Scripts/TargetRotation.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+//angleのスピードで、とあるオブジェクトの周りを回転するスクリプト
+public class TargetRotation : MonoBehaviour
+{	Rigidbody rbody;
+	float speed;
+	float radius;
+	float yPosition;
+	Vector3 TargetPos;
+
+	//一秒当たりの回転角度
+	public  float angle = 30f;
+ 	//回転の中心をとるために使う変数
+	private Vector3 targetPos;
+	//ロックオンしている敵
+	GameObject Obj;
+	//
+	Transform targetTransform;
+
+	//オブジェクトが生まれた瞬間に1度だけ呼ばれる
+	void Start () {
+		//targetを探す
+		Obj = GameObject.Find("Enemy");
+		//変数targetPosに位置情報を取得
+		targetPos = Obj.transform.position;
+		//自分の向きをターゲットの正面に向ける
+		transform.LookAt(Obj.transform);
+		//自分をZ軸を中心に0～360でランダムに回転させる
+//		transform.Rotate(new Vector3(0, 0, Random.Range(0,360)),Space.World);
+
+		rbody = GetComponent<Rigidbody>();
+
+		speed = 2.0f;
+		radius = 5.0f;
+		yPosition = 0.0f;
+	}
+
+	//毎フレーム　60FPSなら1秒間に60回 
+	void Update () {
+		//	Sampleを中心に自分を現在の上方向に、毎秒angle分だけ回転する。
+//		Vector3 axis = transform.TransformDirection(Vector3.up);
+//		transform.RotateAround(targetPos, axis ,angle * Time.deltaTime);
+
+		//自分の向きをターゲットの正面に向ける
+		transform.LookAt(Obj.transform);
+		TargetPos = Obj.transform.position;
+
+		//Unityのキー設定は　EDIT -> ProjectSettings -> Input　で設定されている
+		//ここの"Horizontal"や"Vertical"もそこで設定されているキーを見ている
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+		rbody.MovePosition(new Vector3(
+								TargetPos.x + radius * Mathf.Sin(Time.time*speed),
+								TargetPos.y,
+								TargetPos.z + radius * Mathf.Cos(Time.time*speed)
+								));
+	}
+}
